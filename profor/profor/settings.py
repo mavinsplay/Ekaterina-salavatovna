e63d2fe91bcbc5b5220d01508cae9745d915d687
o2o <Mavinsplay@yandex.ru>
2025-09-24 00:53:30 +0300

    fix 5

--- profor/profor/settings.py
+++ profor/profor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kh$)@0a55(-w&o+_x1a=+ci$qe)0eh5q)webg9qqhxe_gb4uxj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["careerpiter.ru", "www.careerpiter.ru"]
+ALLOWED_HOSTS = ["careerpiter.ru", "www.careerpiter.ru", "127.0.0.1", "localhost"]
 
 
 # Application definition
